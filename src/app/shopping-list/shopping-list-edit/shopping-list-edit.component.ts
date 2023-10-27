a9d21a3cef51ffff1508d31049231dfb243d8f76
beattris <beatriceebirim@gmail.com>
2023-10-28 00:58:41 +0100

    created the necessary components

---
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list-edit',
+  templateUrl: './shopping-list-edit.component.html',
+  styleUrls: ['./shopping-list-edit.component.css']
+})
+export class ShoppingListEditComponent {
+
+}
