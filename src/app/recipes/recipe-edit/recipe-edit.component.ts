5c40cff8cc3524e8d91828b74574a5cce655af89
beattris <beatriceebirim@gmail.com>
2023-12-13 00:54:38 +0100

    retrieved the route parameters to get the id of recipe to be edited

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent {
+export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode = false;
 
+  constructor(private route: ActivatedRoute){}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.editMode = params['id'] != null;
+      console.log(this.editMode)
+    }) 
+  }
 }
