824d0ad5e207193aad899c04b730643aa45f70e1
beattris <beatriceebirim@gmail.com>
2024-01-03 16:02:27 +0100

    set up a dataStorage service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -31,7 +32,7 @@ import { RecipeService } from './recipes/recipe.service';
     RecipesStartComponent,
     RecipeEditComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule],
   providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent],
 })
