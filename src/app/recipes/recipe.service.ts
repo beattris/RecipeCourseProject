7221f568c940745bcd5d5d62e8648be7b0c26471
beattris <beatriceebirim@gmail.com>
2024-01-03 17:05:52 +0100

    transformed the response data with map()

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,27 +9,29 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
   
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Super Tasty Spaghetti!',
-      'The most tasty Spaghetti!!',
-      'https://www.foodandwine.com/thmb/tAS-x_IC4ss1cb9EdDpsr0UExdM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/bucatini-with-mushroom-ragu-dandelion-greens-and-tarragon-FT-RECIPE0421-3a5f0d29f7264f5e9952d4a3a51f5f58.jpg',
-      [new Ingredient('Spaghetti', 2), new Ingredient('Tomatoes', 15)]
-    ),
-    new Recipe(
-      'Tasty Schnitzel',
-      'This is a super tasty schnitzel!',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
-      [new Ingredient('Meat', 2), new Ingredient('French Fries', 20)]
-    ),
-    new Recipe(
-      'Big Fat Burger',
-      'Awesome big fat burger!',
-      'https://live.staticflickr.com/65535/50400566357_6ac78a2933_b.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('Buns', 2)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Super Tasty Spaghetti!',
+  //     'The most tasty Spaghetti!!',
+  //     'https://www.foodandwine.com/thmb/tAS-x_IC4ss1cb9EdDpsr0UExdM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/bucatini-with-mushroom-ragu-dandelion-greens-and-tarragon-FT-RECIPE0421-3a5f0d29f7264f5e9952d4a3a51f5f58.jpg',
+  //     [new Ingredient('Spaghetti', 2), new Ingredient('Tomatoes', 15)]
+  //   ),
+  //   new Recipe(
+  //     'Tasty Schnitzel',
+  //     'This is a super tasty schnitzel!',
+  //     'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+  //     [new Ingredient('Meat', 2), new Ingredient('French Fries', 20)]
+  //   ),
+  //   new Recipe(
+  //     'Big Fat Burger',
+  //     'Awesome big fat burger!',
+  //     'https://live.staticflickr.com/65535/50400566357_6ac78a2933_b.jpg',
+  //     [new Ingredient('Meat', 1), new Ingredient('Buns', 2)]
+  //   ),
+  // ];
 
+  private recipes: Recipe[] = [];
+  
   constructor(private shoppingListService: ShoppingListService) {}
 
   setRecipes(recipes: Recipe[]){
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -19,9 +20,23 @@ export class DataStorageService {
       });
   }
 
-  fetchRecipes(){
-    this.http.get<Recipe[]>('https://recipe-book-fc6e4-default-rtdb.firebaseio.com/recipes.json').subscribe(recipes => {
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>(
+        'https://recipe-book-fc6e4-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
+      .subscribe((recipes) => {
         this.recipeService.setRecipes(recipes);
-    });
+      });
   }
 }
