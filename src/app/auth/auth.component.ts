becb73e59d2138c1243eb0f6017e5c82c9cb8f34
beattris <beatriceebirim@gmail.com>
2024-01-05 19:17:02 +0100

    added login logic and handled error

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthResponseData, AuthService } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -11,33 +12,43 @@ export class AuthComponent {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService) {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm) {
-    if (!form.valid){
-        return;
+    if (!form.valid) {
+      return;
     }
     const email = form.value.email;
     const password = form.value.password;
 
+    let authObs: Observable<AuthResponseData>;
+
     this.isLoading = true;
 
-    if(this.isLoginMode){
-        // ....
-    } else{
-        this.authService.signup(email, password).subscribe(responseData => {
-            console.log(responseData);
-            this.isLoading = false;
-        }, errorMessage => {
-            console.log(errorMessage);
-            this.error = errorMessage;
-            this.isLoading = false;
-        });
+    if (this.isLoginMode) {
+      authObs = this.authService.login(email, password);
+    } else {
+      authObs = this.authService.signup(email, password);
     }
+
+    //  USING THE 'authObs' OBSERVABLE TO SUbSCRIBE, SO AS TO AVOID CODE DUPLICATION IN
+    //  THE IF/ELSE CODE BLOCKS ABOVE.
+
+    authObs.subscribe(
+      (responseData) => {
+        console.log(responseData);
+        this.isLoading = false;
+      },
+      (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
 
     form.reset();
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,38 +1,60 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 
-interface AuthResponseData {
-    kind: string;
-    idToken: string;
-    email: string;
-    refreshToken: string;
-    expiresIn: string;
-    localId: string;
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-    constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDVAYAROTJSmJ7TKTUKxkuuSd-d4w0WRkk', 
-    {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDVAYAROTJSmJ7TKTUKxkuuSd-d4w0WRkk',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDVAYAROTJSmJ7TKTUKxkuuSd-d4w0WRkk',
+      {
         email: email,
         password: password,
-        returnSecureToken: true
-    }
-    ).pipe(catchError(errorRes => {
-      let errorMessage = 'An unknown error occured!';
-      if(!errorRes.error || !errorRes.error.error){
-        return throwError(errorMessage);
+        returnSecureToken: true,
       }
-      switch(errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'This email already exists, please change email address';
-          break;
-      }
+    ).pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occured!';
+    if (!errorRes.error || !errorRes.error.error) {
       return throwError(errorMessage);
-    }));
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email already exists, please change email address';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email is not registered';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'The password you entered is incorrect';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
